3ddea3f2840e9e4c9f066dfbdc2c625bd0389dbf
ereek <erich.sanchez@gmail.com>
2019-04-10 22:29:22 -0500

    Coding the loop

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 # File: guessingame.sh
 
+bflag=1
+
 echo "How many files are in the current directory?"
 read response
 
-if [[ $response -gt 4 ]]
-then
-	echo "$response is too high"
-elif [[ $response -lt 4 ]]
-then
-	echo "$response is too low"
-else
-	true
-fi
+while [[ bflag -eq 1 ]]
+do
+	if [[ $response -gt 4 ]]
+	then
+		echo "$response is too high, try again."
+		read response
+	elif [[ $response -lt 4 ]]
+	then
+		echo "$response is too low, try again."
+		read response
+	else
+		bflag=0
+	fi
+
+done
+
+echo "Congratulations, you guessed."
