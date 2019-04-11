47195653dc555b24ded6687629ae9dc24078f879
ereek <erich.sanchez@gmail.com>
2019-04-11 13:02:25 -0500

    Coding a fuction

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 # File: guessingame.sh
 
+function countfiles {
+
+	local num_files=$(ls -1 | wc -l)
+	echo "$num_files"
+}
+
+
 bflag=1
 
 echo "How many files are in the current directory?"
 read response
 
+num_files=$(countfiles)
+
 while [[ bflag -eq 1 ]]
 do
-	if [[ $response -gt 4 ]]
+	if [[ $response -gt $num_files ]]
 	then
 		echo "$response is too high, try again."
 		read response
-	elif [[ $response -lt 4 ]]
+	elif [[ $response -lt $num_files ]]
 	then
 		echo "$response is too low, try again."
 		read response
